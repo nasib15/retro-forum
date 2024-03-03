a89c57475139cdc0d7d1205c97ecdcd04fbfd353
knh-nehal <nasibhossain57@gmail.com>
2024-03-03 15:10:48 +0600

    latest post api integrated

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,8 @@
 const discussSection = document.getElementById("discuss-section");
 const discussLeft = document.getElementById("discuss-left");
 const discussRight = document.getElementById("discuss-right");
+const markReadContainer = document.getElementById("mark-read-container");
+const latestPostsContainer = document.getElementById("latest-posts-container");
 
 // Load data
 
@@ -17,7 +19,7 @@ const loadData = async () => {
 
 const displayData = (allPosts) => {
   allPosts.forEach((post) => {
-    console.log(post);
+    // console.log(post);
     const div = document.createElement("div");
     div.className = "flex flex-col lg:flex-row gap-6 mb-10";
     div.innerHTML = `
@@ -60,7 +62,7 @@ const displayData = (allPosts) => {
                     <p>${post.posted_time} Min</p>
                   </div>
                   <div class="ml-auto">
-                    <button><img src="./images/email 1.png" alt="" /></button>
+                    <button class="read-btn"><img src="./images/email 1.png" alt="" /></button>
                   </div>
                 </div>
               </div>
@@ -70,7 +72,7 @@ const displayData = (allPosts) => {
   });
 
   const div = document.createElement("div");
-  div.className = "flex flex-col lg:flex-row gap-6";
+  div.className = "flex flex-col lg:flex-row gap-10";
   div.innerHTML = `
   <div
               style="
@@ -78,22 +80,13 @@ const displayData = (allPosts) => {
                 background-repeat: no-repeat;
                 background-size: cover;
               "
-              class="p-10 rounded-3xl"
+              class="p-10 rounded-3xl "
             >
-              <div class="flex justify-between">
+              <div class="flex gap-[60px] justify-between">
                 <h4 class="text-xl font-bold">Title</h4>
                 <div class="flex gap-2">
                   <div><img src="./images/tickmark.png" alt="" /></div>
-                  <p>Mark As Read(1)</p>
-                </div>
-              </div>
-              <div class="bg-white p-8 my-4 rounded-2xl flex justify-between">
-                <h5 class="font-bold text-base">
-                  10 Kids Unaware of Their Halloween Costume
-                </h5>
-                <div class="flex items-center pr-2">
-                  <img src="./images/tabler-icon-eye.png" alt="" />
-                  <p>1,568</p>
+                  <p>Mark As Read(0)</p>
                 </div>
               </div>
             </div>
@@ -102,4 +95,62 @@ const displayData = (allPosts) => {
   discussRight.appendChild(div);
 };
 
+// Mark Read
+
+// Latest posts
+
+const latestPosts = async () => {
+  const res = await fetch(
+    "https://openapi.programming-hero.com/api/retro-forum/latest-posts"
+  );
+  const posts = await res.json();
+
+  posts.forEach((post) => {
+    console.log(post);
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <div class="mt-12">
+            <div class="card bg-base-100 shadow-xl">
+              <figure class="px-10 pt-10">
+                <img
+                  src=${post.cover_image}
+                  alt="Shoes"
+                  class="rounded-xl"
+                />
+              </figure>
+              <div class="card-body text-left space-y-2">
+                <h2 class="font-extrabold text-lg">
+                  ${post.title}
+                </h2>
+                <p class="opacity-80">
+                  ${post.description}
+                </p>
+                <div class="flex items-center gap-4">
+                  <div>
+                    <img class="w-[50px] rounded-full" src=${
+                      post.profile_image
+                    } alt="" />
+                  </div>
+                  <div>
+                    <h4 class="text-base font-extrabold">${
+                      post.author.name
+                    }</h4>
+                    <p class="opacity-80">${
+                      post.author.designation
+                        ? post.author.designation
+                        : "unknown"
+                    }</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+    `;
+
+    latestPostsContainer.appendChild(div);
+  });
+};
+
 loadData();
+
+latestPosts();
